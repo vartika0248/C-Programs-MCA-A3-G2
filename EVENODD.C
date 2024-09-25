b631c2dbd712dd5e57627d6d1298df14f9a041ab
vartika0248 <160117582+vartika0248@users.noreply.github.com>
2024-09-25 12:35:40 +0530

    Add files via upload

---
+++ EVENODD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int n;
+ printf("Enter any number:\n");
+ scanf("%d",&n);
+ if(n%2==0)
+ {
+ printf("Number is even");
+ }
+ else
+ {
+ printf("Number is Odd");
+ }
+ getch();
+ clrscr();
+ }
---
+++ SUM_A_B.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b;
+ printf("Enter any two numbers:\n ");
+ scanf("%d %d",&a,&b);
+ printf("Sum of the numbers=%d",a+b);
+ getch();
+ clrscr();
+ }
