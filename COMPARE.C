b3054faf2d20a0428740d615b0b2e7480af552bb
vartika0248 <160117582+vartika0248@users.noreply.github.com>
2024-09-27 12:29:22 +0530

    Add files via upload

---
+++ COMPARE.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a=6,b=10;
+ printf("a=%d and b=%d\n \n",a,b);
+ if(a>b)
+    printf("a is greater.\n");
+ else
+    printf("b is greater.\n");
+ getch();
+ clrscr();
+}
